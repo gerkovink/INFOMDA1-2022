d319058e3bd0a5eed3a1c46ae5204decedc17a9c
Ana Martins <a.i.silvamartins@students.uu.nl>
2022-10-31 11:52:06 +0100

    more on data generating for assignment 2

--- Assignments/Assignment 2/AnaMartins_HsuanLee_TimovanVeghel_Assignment_2/data/data_generating.py
+++ Assignments/Assignment 2/AnaMartins_HsuanLee_TimovanVeghel_Assignment_2/data/data_generating.py
@@ -1,13 +1,23 @@
 import requests
+import json
+
+from pandas.io.json import json_normalize
  
-url = "https://imdb-api.com/API/AdvancedSearch/k_vg4dpwb1?title_type=feature"
+url = "https://imdb-api.com/API/AdvancedSearch/k_vg4dpwb1?title=a&title_type=feature&count=250&sort=alpha,asc"
 
 payload = {}
 headers= {}
  
 response = requests.request("GET", url, headers=headers, data = payload)
+
+todos = json.loads(response.text)
+
+df = json_normalize(todos, 'results')
+df.to_csv("output.csv", index=False, sep='\t', encoding="utf-8")
+
+print(todos)
  
 while open("data.json", "w").write(response.text):
     print("Data saved to file")
     break
-print(response.text.encode('utf8'))
+#print(response.text.encode('utf8'))
