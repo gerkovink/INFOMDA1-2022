5247f610f76aa622e341468aa9e1cbe4a9ec02e9
ana <anaisabelmartins@tecnico.ulisboa.pt>
2022-11-01 20:50:54 +0100

    finished connection to api?

--- Assignments/Assignment 2/AnaMartins_HsuanLee_TimovanVeghel_Assignment_2/data/data_generating.py
+++ Assignments/Assignment 2/AnaMartins_HsuanLee_TimovanVeghel_Assignment_2/data/data_generating.py
@@ -1,23 +1,39 @@
 import requests
+import csv
 import json
 
-from pandas.io.json import json_normalize
+data_file = open('data_file.csv', 'w')
  
-url = "https://imdb-api.com/API/AdvancedSearch/k_vg4dpwb1?title=a&title_type=feature&count=250&sort=alpha,asc"
+count = True
 
-payload = {}
-headers= {}
- 
-response = requests.request("GET", url, headers=headers, data = payload)
+# set up
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+asc = True
+data_file = open('data_file.csv', 'a')
+csv_writer = csv.writer(data_file)
 
-todos = json.loads(response.text)
+for letter in alphabet:
+    for i in range(0, 2):
+        if asc:
+            url = "https://imdb-api.com/API/AdvancedSearch/k_vg4dpwb1?title=" + letter + "a&title_type=feature&count=250&sort=alpha,asc"
+            asc = False
+        else:
+            url = "https://imdb-api.com/API/AdvancedSearch/k_vg4dpwb1?title=" + letter + "a&title_type=feature&count=250&sort=alpha,desc"
+            asc = True
 
-df = json_normalize(todos, 'results')
-df.to_csv("output.csv", index=False, sep='\t', encoding="utf-8")
+        response = requests.request("GET", url)
+        data = json.loads(response.text)
 
-print(todos)
- 
-while open("data.json", "w").write(response.text):
-    print("Data saved to file")
-    break
-#print(response.text.encode('utf8'))
+        movies = data['results']
+
+        for movie in movies:
+            if count:
+        
+                # Writing headers of CSV file
+                header = movie.keys()
+                csv_writer.writerow(header)
+                count = False
+        
+            # Writing data of CSV file
+            if movie.values() != None:
+                csv_writer.writerow(movie.values())
